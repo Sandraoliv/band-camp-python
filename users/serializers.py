ea72a97f017a392c15e985a45dcfc5f3b59335ee
sandraoliv <sandra.so219@gmail.com>
2023-06-20 17:13:14 -0300

    feat:Tarefa 1 finalizada

--- users/serializers.py
+++ users/serializers.py
@@ -3,30 +3,39 @@ from rest_framework.validators import UniqueValidator
 from .models import User
 
 
-class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(
         validators=[
             UniqueValidator(
                 queryset=User.objects.all(),
-                message="A user with that username already exists.",
+                message="A user with that username already exists."
             )
-        ],
+        ]
     )
     email = serializers.EmailField(
-        validators=[UniqueValidator(queryset=User.objects.all())],
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="This field must be unique."
+            )
+        ]
     )
-    password = serializers.CharField(write_only=True)
-    full_name = serializers.CharField(max_length=50, required=False)
-    artistic_name = serializers.CharField(max_length=50)
 
     def create(self, validated_data: dict) -> User:
-        return User.objects.create_user(**validated_data)
+        return User.objects.create_user(**validated_data) 
+    
+    def update(self, instance, validated_data):
+       
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
 
-    def update(self, instance: User, validated_data: dict) -> User:
-        for key, value in validated_data.items():
-            setattr(instance, key, value)
+        return super().update(instance, validated_data)
+    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'full_name', 'artistic_name']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
-        instance.save()
-
-        return instance
